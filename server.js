a151c731699f3f81fec8d0c1f9189187bfc07544
Vitalii S. <svitalii138@gmail.com>
2025-03-21 00:59:05 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 const fs = require("fs");
 const cron = require("node-cron");
 
@@ -11,6 +11,24 @@ const API_URL = process.env.API_URL;
 const LOGIN = process.env.LOGIN;
 const PASSWORD = process.env.PASSWORD;
 
+axios.defaults.baseURL = API_URL;
+
+const getAuthToken = async () => {
+  const res = await axios.get(`/api2/login/?username=${LOGIN}&password=${PASSWORD}`);
+  console.log("auth data", res.data);
+  return res.data.accessToken;
+};
+
+const fetchDataBase = async () => {
+  const token = await getAuthToken();
+  const startDate = new Date().toISOString().split("T")[0];
+  const res = await axios.get(`/api2/appointments/report?StartDateFrom=${startDate}&_limit=50`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log(res.data);
+};
+
 app.listen(PORT, () => {
+  fetchDataBase();
   console.log("Server is running!");
 });
