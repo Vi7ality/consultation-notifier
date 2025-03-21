5affaa928ca2ee0e024e7dacc7fd0a3cd74de615
Vitalii S. <svitalii138@gmail.com>
2025-03-22 00:35:01 +0200

    cron added to schedule checks

--- server.js
+++ server.js
@@ -21,15 +21,14 @@ const getAuthToken = async () => {
 const fetchDataBase = async () => {
   const token = await getAuthToken();
   const startDate = new Date().toISOString().split("T")[0];
-  const res = await axios.get(`/api2/appointments/report?StartDateFrom=${startDate}&_limit=50`, {
+  const res = await axios.get(`/api2/appointments/report?StartDateFrom=${startDate}&_limit=100`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
 };
 
-const getVisitorList = async () => {
-  const data = await fetchDataBase();
-  const visitors = data.map((client) => {
+const getRecordsList = async (data) => {
+  const records = data.map((client) => {
     const clientData = {
       name: client.client.name,
       email: client.client.email,
@@ -42,11 +41,32 @@ const getVisitorList = async () => {
     };
     return clientData;
   });
-  console.log("visitors", visitors);
-  return visitors;
+  return records;
 };
+
+const filterByCreateDate = (data, date) => {
+  // const currentDate = new Date();
+  const filterDate = new Date(date);
+  return data.filter((item) => new Date(item.createDate) > filterDate);
+};
+
+const prevDate = "2025-03-19T15:58:50.673";
+
+const checkAndSendEmails = async () => {
+  const data = await fetchDataBase();
+  const recordList = await getRecordsList(data);
+  const filteredRecordList = filterByCreateDate(recordList, prevDate);
+  console.log("Found records", filteredRecordList);
+  filteredRecordList.forEach(({ email }) => {
+    console.log(`Send email to ${email}`);
+  });
+};
+
+cron.schedule("*/1 * * * *", async () => {
+  console.log("Check API data", new Date());
+  await checkAndSendEmails();
+});
 
 app.listen(PORT, () => {
   console.log("Server is running!");
-  getVisitorList();
 });
