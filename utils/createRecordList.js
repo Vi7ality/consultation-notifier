e89f4ff3b2dd4c76bdd724b8ccaace281e6a1974
Vitalii S. <svitalii138@gmail.com>
2025-05-25 00:05:02 +0300

    add client checking to createRecordList

--- utils/createRecordList.js
+++ utils/createRecordList.js
@@ -1,17 +1,22 @@
 const formatDate = require("./formatDate");
 
 const createRecordList = (data) =>
-  data.map(({ client, startDate, createDate, resultProcedures, courses }) => ({
-    name: client.name,
-    firstName: client.firstName,
-    middleName: client.middleName,
-    lastName: client.lastName,
-    email: client.email,
-    phone: `+38${client.phone}`,
-    birthDate: client.birthDate,
-    eventDate: formatDate(startDate),
-    createDate: formatDate(createDate),
-    cause: resultProcedures[0]?.procedureName || courses[0]?.title || "",
-  }));
+  data.reduce((acc, { client, startDate, createDate, resultProcedures, courses }) => {
+    if (client && startDate) {
+      acc.push({
+        name: client.name || client.firstName || "",
+        firstName: client.firstName || "",
+        middleName: client.middleName || "",
+        lastName: client.lastName || "",
+        email: client.email || "",
+        phone: `+38${client.phone}` || "",
+        birthDate: client.birthDate || "",
+        eventDate: formatDate(startDate),
+        createDate: formatDate(createDate),
+        cause: resultProcedures[0]?.procedureName || courses[0]?.title || "",
+      });
+    }
+    return acc;
+  }, []);
 
 module.exports = createRecordList;
